aeb7fb2efd3f8f3abb7321c113bea3b7d4032f3c
Jaime Nguyen <jaimenguyen168@gmail.com>
2025-08-28 18:40:07 -0400

    Add convex & tabbar layout

---
+++ src/app/(app)/(tabs)/activity.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const ActivityScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-blue-200">
+      <Text>ActivityScreen</Text>
+    </View>
+  );
+};
+export default ActivityScreen;
